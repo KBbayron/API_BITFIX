c22220f8a6cb6548e00543c46c12cd6c8901a56e
KBbayron <bayronlr@outlook.com>
2025-02-12 16:34:41 -0600

    crea migrations y arregla index de models

---
+++ src/DB/migrations/20250212222823-MigrationInit.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const { perfilSchema } = require('./perfil.model');
+const { consultationsSchema } = require('./consultations.model');
+const { devicesSchema } = require('./device.model');
+const { prioritiesSchema } = require('./priority.model');
+const { repairsSchema } = require('./repairs.model');
+const { rolesSchema } = require('./rol.model');
+const { ticketsSchema } = require('./ticket.model');
+const { usersSchema } = require('./user.model');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.createTable('perfiles', perfilSchema, { transaction });
+      await queryInterface.createTable('consultations', consultationsSchema, { transaction });
+      await queryInterface.createTable('devices', devicesSchema, { transaction });
+      await queryInterface.createTable('priorities', prioritiesSchema, { transaction });
+      await queryInterface.createTable('repairs', repairsSchema, { transaction });
+      await queryInterface.createTable('roles', rolesSchema, { transaction });
+      await queryInterface.createTable('tickets', ticketsSchema, { transaction });
+      await queryInterface.createTable('users', usersSchema, { transaction });
+
+      await transaction.commit(); // Confirma y guarda los cambios realizados
+    } catch (error) {
+      await transaction.rollback();// Revierte todos los cambios realizados
+      throw error;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.dropTable('users', { transaction });
+      await queryInterface.dropTable('tickets', { transaction });
+      await queryInterface.dropTable('roles', { transaction });
+      await queryInterface.dropTable('repairs', { transaction });
+      await queryInterface.dropTable('priorities', { transaction });
+      await queryInterface.dropTable('devices', { transaction });
+      await queryInterface.dropTable('consultations', { transaction });
+      await queryInterface.dropTable('perfiles', { transaction });
+
+      await transaction.commit();// Revierte todos los cambios realizados
+    } catch (error) {
+      await transaction.rollback();// Revierte todos los cambios realizados
+      throw error;
+    }
+  }
+};
--- src/DB/models/index.js
+++ src/DB/models/index.js
@@ -1,21 +1,21 @@
 const {Perfil, perfilSchema} = require('./perfil.model');
-const {Consultation, consultationschema} = require('./consultations.model');
+const {Consultation, constationsSchema} = require('./consultations.model');
 const {Device, devicesSchema} = require('./device.model');
 const {Priority, prioritiesSchema} = require('./priority.model');
 const {Repair, repairsSchema} = require('./repairs.model');
 const {Rol, rolesSchema} = require('./rol.model');
 const {Ticket, ticketsSchema} = require('./ticket.model');
-const {User, userschema} = require('./user.model');
+const {User, usersSchema} = require('./user.model');
 
 const models = [
   { model: Perfil, schema: perfilSchema },
-  { model: Consultation, schema: consultationschema },
+  { model: Consultation, schema: constationsSchema },
   { model: Device, schema: devicesSchema },
   { model: Priority, schema: prioritiesSchema },
   { model: Repair, schema: repairsSchema },
   { model: Rol, schema: rolesSchema },
   { model: Ticket, schema: ticketsSchema },
-  { model: User, schema: userschema }
+  { model: User, schema: usersSchema }
 ];
 
 module.exports = models;
